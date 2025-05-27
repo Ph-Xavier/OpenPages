4d30734d25a3fb50242ad706c7d9a9799af1eb1a
Pedro Xavier <pedroxxavier@hotmail.com>
2025-05-27 13:41:39 -0300

    Remoção de dependência que não está sendo usada.

--- projeto/app.js
+++ projeto/app.js
@@ -1,7 +1,6 @@
 let createError = require("http-errors");
 let express = require("express");
 let path = require("path");
-let cookieParser = require("cookie-parser");
 let logger = require("morgan");
 
 let indexRouter = require("./routes/indexRouter");
@@ -21,7 +20,6 @@ app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 
